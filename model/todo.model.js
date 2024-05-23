a6e933a09255772b6e09e8825ed6ac2d19306be5
haydeeMugisha <haydee.mugisha@hicoders.ch>
2024-05-24 00:36:43 +0200

    added todo model

--- model/todo.model.js
+++ model/todo.model.js
@@ -2,17 +2,23 @@ import sequelize from "../config/database.config.js";
 import { DataTypes } from "sequelize";
 
 const Todo = sequelize.define("Todo", {
-   
-    email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        isEmail: {
-            msg: 'Email is required'
-        },
-    }
-},{
-    tableName: 'todo'
+  description: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  motivationalMessage: {
+    type: DataTypes.STRING,
+  },
+  status: {
+    type: DataTypes.STRING,
+    defaultValue: 'active',
+  },
+  userId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+}, {
+  tableName: 'todo',
 });
 
-
-export {Todo}
+export { Todo };
